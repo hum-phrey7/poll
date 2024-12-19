8ea48dfd4fd06638cedb2cb4372da4bcb63c6d7c
Humphrey.... <153418064+hum-phrey7@users.noreply.github.com>
2024-12-19 20:31:56 +0000

    Add files via upload

---
+++ pol.js
@@ -0,0 +1,40 @@
+
+
+const poll = {
+    question: 'what is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    
+    answers: new Array(4).fill(0),
+
+    registerNewAnswer() {
+         const answer = Number( prompt(`${this.question}\n${this.options.join('\n')}
+        \n(Write option number)` )
+     );
+     console.log(answer)
+
+     typeof answer === 'Number' && answer < this.answers.length && 
+     this.answers[answer]++;
+
+     this.displayResults();
+     this.displayResults('string');
+ },
+      displayResults(type = 'array') 
+      {
+        if(type ==='array'){
+            console.log(this.answers);
+        }else if (type === 'string'){
+            console.log(`Poll results are ${this.answers.join(', ')}`);
+
+        }
+      }
+
+   
+};
+
+ //poll.registerNewAnswer();
+
+ document.querySelector('.poll')
+ .addEventListener('click', poll.registerNewAnswer.bind(poll));
+
+
+
